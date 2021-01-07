98e277a407dc16775080bc35e64888ccf3be45cb
Martin Kim <martinjwkim@gmail.com>
2021-01-07 15:23:51 -0800

    radix sort

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,4 @@
 function test(num=5,num2){
-  console.log(num, num2)
 }
 
 test(undefined,1)
@@ -8,4 +7,7 @@ test(2,1)
 function factorial(num){
   if (num === 1) return 1;
   return num * factorial(num-1)
+}
+
+function practicingSpread(arr){
 }
---
+++ sorting/radixSort.js
@@ -0,0 +1,33 @@
+function radixSort(arr) {
+  let maxDigits = mostDigits(arr)
+
+  for (let i = 0; i < maxDigits; i++) {
+    let buckets = Array.from({ length: 10 }, () => []);
+    for (let j = 0; j < arr.length; j++) {
+      let digit = getDigit(arr[j], i);
+      buckets[digit].push(arr[j]);
+    }
+    arr = [].concat(...buckets);
+  }
+
+  return arr;
+}
+
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let num of nums) {
+    maxDigits = Math.max(maxDigits, digitCount(num))
+  }
+  return maxDigits;
+}
+
+console.log(radixSort([5,1,37,5312,2351,4]))
