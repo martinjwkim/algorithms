73e04f6c0be46cecb3d70079007ed0b3d3d18e57
Martin Kim <martinjwkim@gmail.com>
2021-01-07 20:11:41 -0800

    enuf for today my head hurts

--- data-structures/singlyLinkedList.js
+++ data-structures/singlyLinkedList.js
@@ -12,7 +12,16 @@ class SinglyLinkedList{
     this.length = 0;
   }
   push(val){
-
+    var newNode = new Node(val)
+    if(!this.head){
+      this.head=newNode;
+      this.tail=this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
   }
 }
 
