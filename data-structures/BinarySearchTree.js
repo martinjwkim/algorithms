28455fea3b67dfe78a3cb97dc72cdc8bfaa2b4c3
Martin Kim <martinjwkim@gmail.com>
2021-01-13 16:28:50 -0800

    DFSInOrder

--- data-structures/BinarySearchTree.js
+++ data-structures/BinarySearchTree.js
@@ -119,6 +119,17 @@ class BinarySearchTree {
     return data;
   }
 
-  
+  DFSInOrder(){
+    let data = [];
+
+    function traverse(node){
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
 }
 
