9c5ad0c2004605aa52083a3a1510af608e986230
Martin Kim <martinjwkim@gmail.com>
2021-01-13 16:19:22 -0800

    DFSPreOrder and DFSPostOrder

--- data-structures/BinarySearchTree.js
+++ data-structures/BinarySearchTree.js
@@ -92,5 +92,33 @@ class BinarySearchTree {
 
     return data;
   }
+
+  DFSPreOrder(){
+    let data = [];
+
+    function traverse(node){
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
+
+  DFSPostOrder(){
+    let data = [];
+
+    function traverse(node){
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    
+    traverse(this.root);
+    return data;
+  }
+
+  
 }
 
