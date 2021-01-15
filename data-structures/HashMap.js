9d1ccbfb0398c9dfcb1ae1217de7d04d897500bd
Martin Kim <martinjwkim@gmail.com>
2021-01-14 20:12:51 -0800

    keys and values to hash table

--- data-structures/HashMap.js
+++ data-structures/HashMap.js
@@ -33,4 +33,31 @@ class HashTable {
     }
     return undefined;
   }
+
+  key(){
+    let keysArr=[];
+    for(let i=0;i<this.keyMap.length;i++){
+      if(this.keyMap[i]){
+        for(let j=0; j<this.keyMap[i].length; j++){
+          if(!keysArr.includes(this.keyMap[i][j][0])){
+            keysArr.push(this.keyMap[i][j][0])
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+
+  values(){
+    let valuesArr = [];
+    for (let i=0; i < this.keyMap.length; i++){
+      if (this.keyMap[i]){
+        for (let j=0; j<this.keyMap[i].length; j++){
+          if(!vaulesArr.includes(this.keyMap[i][j][1]))
+          valuesArr.push(this.keyMap[i][j][1]);
+        }
+      }
+    }
+    return valuesArr;
+  }
 }
