93d6126f8166f4cd47af0b611d26fa9e8ab57571
Martin Kim <martinjwkim@gmail.com>
2021-01-14 18:49:24 -0800

    hash get

--- data-structures/HashMap.js
+++ data-structures/HashMap.js
@@ -23,6 +23,14 @@ class HashTable {
   }
   
   get(key){
-    
+    let index = this._hash(key);
+    if(this.keyMap[index]){
+      for (let i=0; i<this.keyMap[index].length; i++){
+        if (this.keyMap[index][i][0]===key){
+          return this.keyMap[index][i];
+        }
+      }
+    }
+    return undefined;
   }
 }
