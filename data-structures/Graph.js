8aeeca53a9f7fb03db66773c1e41fb10cc21d1f2
Martin Kim <martinjwkim@gmail.com>
2021-01-18 13:03:10 -0800

    weight graph class

--- data-structures/Graph.js
+++ data-structures/Graph.js
@@ -88,19 +88,3 @@ class Graph {
     return result;
   }
 }
-
-let g = new Graph;
-g.addVertex("A")
-g.addVertex("B")
-g.addVertex("C")
-g.addVertex("D")
-g.addVertex("E")
-g.addVertex("F")
-g.addEdge("A","B")
-g.addEdge("A","C")
-g.addEdge("B","D")
-g.addEdge("C","E")
-g.addEdge("D","E")
-g.addEdge("D","F")
-g.addEdge("E","F")
-console.log(g.BFS("A"));
