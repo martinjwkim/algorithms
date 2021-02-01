992705cccaf12ba4dc78475711f09c91ac5b8b3e
Martin Kim <martinjwkim@gmail.com>
2021-02-01 13:16:05 -0800

    rotate outer

--- leetcode/rotateImage.js
+++ leetcode/rotateImage.js
@@ -5,5 +5,31 @@ DO NOT allocate another 2D matrix and do the rotation.
 */ 
 
 const rotateImage = (matrix) => {
-  
+  // outer box
+  // inner box
+  function rotateOuter(m) {
+    // collect clockwise in arr
+    // top
+    let arr = [...m[0]];
+    // right going down
+    for (let i=1 ; i<m.length-1; i++){
+      arr.push(m[i][m[i].length-1]);
+    }
+    // bot in reverse
+    arr.push(...m[m.length-1].slice().reverse());
+    // left going up
+    for (let i=m.length-2; i>0; i--){
+      arr.push(m[i][0])
+    }
+
+    for (let i=0; i<matrix.length-1; i++){
+      arr.unshift(arr.pop())
+    }
+
+    return  arr;
+  }
+
+  return rotateOuter(matrix)
 }
+
+console.log(rotateImage([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
