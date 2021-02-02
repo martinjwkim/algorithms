4d57aaf1de11ea55f819936b966a3cf937cc3583
Martin Kim <martinjwkim@gmail.com>
2021-02-01 20:47:06 -0800

    jump game ugly

---
+++ leetcode/jumpGame.js
@@ -0,0 +1,35 @@
+/*
+Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+*/
+
+const canJump = (nums) => {
+  let deadEnds = {};
+  let startIndex = nums.indexOf(0) - 1;
+  if (startIndex === -2 || nums.length === 1) return true;
+
+  function jump(arr, currIndex, path) {
+    if (currIndex + arr[currIndex] >= arr.length - 1) return true;
+    if (deadEnds[currIndex]) return false;
+    if (arr[currIndex] === 0) {
+      for (let i = 0; i < path.length; i++) {
+        deadEnds[path[i]] = true;
+      }
+      return false;
+    }
+    for (let i = arr[currIndex]; i > 0; i--) {
+      if (jump(arr, currIndex + i, path.concat(currIndex + i))) return true;
+    }
+    return false;
+  }
+
+  if (startIndex>=0){
+    for (let i = startIndex; i >= 0; i--) {
+      if (jump(nums, i, [i])) return true;
+    }
+    return false;
+  } else {
+    return jump(nums,0,[])
+  }
+}
