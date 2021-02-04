2854a77707dfa8795f3fc43af7ff32eb8e951a47
Martin Kim <martinjwkim@gmail.com>
2021-02-03 17:07:11 -0800

    uniquepaths2 optimized

--- leetcode/uniquePaths.js
+++ leetcode/uniquePaths.js
@@ -1,22 +1,11 @@
 const uniquePaths = (m, n) => {
 
   function traverse(x, y, memo) {
-
-    if (memo[`${x},${y}`]) {
-      return memo[`${x},${y}`];
-    }
-
-    if (x === 1 & y === 1) {
-      return 1;
-    }
-
-    if (x === 0 || y === 0) {
-      return 0;
-    }
-
+    if (memo[`${x},${y}`]) return memo[`${x},${y}`];
+    if (x === 1 & y === 1) return 1
+    if (x === 0 || y === 0) return 0;
     memo[`${x},${y}`] = traverse(x - 1, y, memo) + traverse(x, y - 1, memo);
     return memo[`${x},${y}`]
-
   }
 
   return traverse(m, n, {});
--- leetcode/uniquePaths2.js
+++ leetcode/uniquePaths2.js
@@ -1,3 +1,15 @@
 const uniquePathsWithObstacles = (obstacleGrid) => {
-    
+
+  function traverse(x, y, memo) {
+    if (memo[`${x},${y}`]) return memo[`${x},${y}`]
+    if (x === 0 || y === 0) return 0;
+    if (obstacleGrid[x-1][y-1]===1) return 0;
+    if (x === 1 & y === 1) return 1;
+    memo[`${x},${y}`] = traverse(x - 1, y, memo) + traverse(x, y - 1, memo);
+    return memo[`${x},${y}`]
+  }
+
+  return traverse(obstacleGrid.length, obstacleGrid[0].length, {});
 };
+
+console.log(uniquePathsWithObstacles([[1]]))
