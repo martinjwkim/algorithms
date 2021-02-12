9fb60693f06137a4b86093b0d11608bd09e3fe14
Martin Kim <martinjwkim@gmail.com>
2021-02-11 17:43:23 -0800

    find minimum in rotated sorted array

--- leetcode/findMinimumInRotatedSortedArray.js
+++ leetcode/findMinimumInRotatedSortedArray.js
@@ -1,3 +1,12 @@
 const findMin = (nums) => {
-  
+  let min = nums[0];
+
+  for (let i=0; i<nums.length; i++){
+    if (nums[i] < min) {
+      min = nums[i];
+      break;
+    }
+  }
+
+  return min;
 }
