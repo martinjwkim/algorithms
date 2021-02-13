7d82d4c9749defdd493a398f9e253b7f2c0d38ba
Martin Kim <martinjwkim@gmail.com>
2021-02-12 21:13:05 -0800

    longest increasing substring

--- leetcode/longestIncreasingSubstring.js
+++ leetcode/longestIncreasingSubstring.js
@@ -1,3 +1,13 @@
 const lengthOfLIS = (nums) => {
-  
+  if(nums.length === 0) return 0
+    let dp = new Array(nums.length).fill(1)
+    for(let i = 1; i < dp.length; i++) {
+        for(let j = 0; j < i; j++) {
+            if(nums[j] < nums[i]){
+                dp[i] = Math.max(dp[i], 1 + dp[j])
+            }
+        }
+    }
+    
+    return Math.max(...dp)
 }
