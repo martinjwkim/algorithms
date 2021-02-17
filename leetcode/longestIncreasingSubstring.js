7219d09ce62feeab71d5334c238434c19e70962c
Martin Kim <martinjwkim@gmail.com>
2021-02-17 14:01:57 -0800

    word break unoptimized

--- leetcode/longestIncreasingSubstring.js
+++ leetcode/longestIncreasingSubstring.js
@@ -1,18 +1,17 @@
 const lengthOfLIS = (nums) => {
   let arr = [];
 
-    for (let i=0; i<nums.length; i++){
-        let inserted = false;
-        
-        for (let j=0; j<arr.length; j++){
-            
-            if (nums[i]<=arr[j] && !inserted) {
-                arr[j] = nums[i]
-                inserted = true;
-            }
-        }
-        if (!inserted) arr.push(nums[i])
+  for (let i = 0; i < nums.length; i++) {
+    let inserted = false;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (nums[i] <= arr[j] && !inserted) {
+        arr[j] = nums[i];
+        inserted = true;
+      }
     }
-    
-    return arr.length
+    if (!inserted) arr.push(nums[i]);
+  }
+
+  return arr.length
 }
--- leetcode/wordBreak.js
+++ leetcode/wordBreak.js
@@ -1,22 +1,14 @@
 const wordBreak = (s, wordDict) => {
-  const map = {};
-    function run(s) {
-        if (map.hasOwnProperty(s)) {
-            return map[s]
-        } else if (s=='') {
-            return true
-        }
-        for (let i=0;i<=s.length;i++) {
-            if (
-                wordDict.includes(s.slice(i)) &&
-                run(s.slice(0, i))
-            ){
-                map[s] = true
-                return true
-            }
-        }
-        map[s] = false
-        return false
-    }
-    return run(s)
-}
+  if (s.length === 0) return true;
+  
+  for (let word of wordDict){
+      let len = word.length;
+      if (s.slice(0,len) === word){
+          if(wordBreak(s.slice(len), wordDict)) {
+              return true;
+          }
+      }
+  }
+  
+  return false;
+};
