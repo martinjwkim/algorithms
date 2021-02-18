04cc264a3b68579f93ef3d5d392fb443a8d026f3
Martin Kim <martinjwkim@gmail.com>
2021-02-18 14:28:56 -0800

    house robber 2 optimized

--- leetcode/houserobber2.js
+++ leetcode/houserobber2.js
@@ -1,10 +1,16 @@
 const rob = (nums) => {
 
-  nums.push(nums[0]);
+  if (nums.length === 1) return nums[0]
 
-  for (let i = nums.length - 1; i >= 0; i--) {
-    nums[i] += Math.max(nums[i + 2] || 0, nums[i + 3] || 0)
+  let nums1 = nums.slice(1);
+  let nums2 = nums.slice(0, nums.length - 1);
+
+  const robHouses = (nums) => {
+    for (let i = nums.length - 1; i >= 0; i--) {
+      nums[i] += Math.max(nums[i + 2] || 0, nums[i + 3] || 0)
+    }
+    return Math.max(nums[0] || 0, nums[1] || 0)
   }
 
-  return Math.max(nums[0] || 0, nums[1] || 0)
+  return Math.max(robHouses(nums1), robHouses(nums2))
 }
