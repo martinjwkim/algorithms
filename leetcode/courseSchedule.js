8423587076d94fe6267d52a69a07a842fb8611d8
Martin Kim <martinjwkim@gmail.com>
2021-02-19 22:50:09 -0800

    num islands

--- leetcode/courseSchedule.js
+++ leetcode/courseSchedule.js
@@ -1,32 +1,19 @@
 var canFinish = function(numCourses, prerequisites) {
-  function takePrereq(course, graph, count){
-      if (count >  numCourses) return count;
-      if (graph[course] === undefined || !graph[course].length) return 1;
-      
-      for (let prereq of graph[course]){
-          count+=takePrereq(prereq, graph, count)
-      }
-      
-      visited[course] = true;
-      
-      return count+1;
-  }
-
-  let adjList = {};
   let visited = {};
+  let adjList = {};
 
   for (let relation of prerequisites) {
-      adjList[relation[0]] = relation.slice(1);
+    adjList[relation[0]] = relation.slice(1); 
   }
 
-  console.log(adjList)
-  
-  let num = 0;
-  for (let node in adjList) {
-      if (!visited[node]) num+=takePrereq(node, adjList, num);
+  for (let node of adjList) {
+    if (!visited[node]){
+      if (!traverse()) return false;
+    }
   }
-  
-  return num > numCourses ? false : true;
+
+  return true;
+
 };
 
 console.log(canFinish(2,[[1,0]]))
