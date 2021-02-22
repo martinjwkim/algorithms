cd0388a4d450e793fd997beb243860b71b969fd8
Martin Kim <martinjwkim@gmail.com>
2021-02-22 08:19:15 -0800

    course schedule with backtracking and memoization

--- leetcode/courseSchedule.js
+++ leetcode/courseSchedule.js
@@ -1,19 +1,37 @@
-var canFinish = function(numCourses, prerequisites) {
-  let visited = {};
-  let adjList = {};
+const canFinish = (numCourses, prerequisites) => {
 
-  for (let relation of prerequisites) {
-    adjList[relation[0]] = relation.slice(1); 
+  const traverse = (course, courseGraph, visited, courseMemo) => {
+    if (visited[course]) return false;
+    if (courseMemo[course]) return true;
+
+    visited[course] = true;
+
+    for (let neighbor of courseGraph[course]) {
+      if (!traverse(neighbor, courseGraph, visited, courseMemo)) return false;
+    }
+
+    visited[course] = false;
+
+    courseMemo[course] = true;
+
+    return true;
   }
 
-  for (let node of adjList) {
-    if (!visited[node]){
-      if (!traverse()) return false;
-    }
+  let adjList = {};
+  let memo = {};
+  let obj = {};
+
+  for (let i = 0; i < numCourses; i++) {
+    adjList[i] = [];
+  }
+
+  for (let relation of prerequisites) {
+    adjList[relation[0]].push(relation[1]);
+  }
+
+  for (let node in adjList) {
+    if (!traverse(node, adjList, obj, memo)) return false;
   }
 
   return true;
-
 };
-
-console.log(canFinish(2,[[1,0]]))
