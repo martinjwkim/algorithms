86931aafc21a93acdd5843469362db7e03714e87
Martin Kim <martinjwkim@gmail.com>
2021-02-23 14:38:42 -0800

    erase overlap intervals

--- leetcode/longestConsecutiveSequence.js
+++ leetcode/longestConsecutiveSequence.js
@@ -1,3 +1,15 @@
-const longestConsecutive = (nums) => {
-  
+const eraseOverlapIntervals = (nums) => {
+  let sortedNums = nums.sort((a, b) => a[1] - b[1]);
+  let count = 0;
+  let curr = sortedNums[0];
+
+  for (let i = 1; i < sortedNums.length; i++) {
+    if (curr[1] > sortedNums[i][0]){
+      count++;
+    } else {
+      curr = sortedNums[i];
+    }
+  }
+
+  return count;
 }
