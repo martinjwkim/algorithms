e50caa3182172e8979d9fbe24d723477d8d93cd6
Martin Kim <martinjwkim@gmail.com>
2021-02-23 16:46:52 -0800

    merge two sorted lists

---
+++ leetcode/mergeTwoSortedLists.js
@@ -0,0 +1,15 @@
+const mergeTwoLists = (l1, l2, curr = new ListNode(0)) => {
+  if (!l1 && !l2) return l1;
+
+  if (!l1 || !l2) {
+    curr.next = l2 || l1;
+  } else if (l1.val < l2.val) {
+    curr.next = l1;
+    mergeTwoLists(l1.next, l2, l1)
+  } else if (l1.val >= l2.val) {
+    curr.next = l2;
+    mergeTwoLists(l1, l2.next, l2)
+  }
+
+  return curr.next;
+}
