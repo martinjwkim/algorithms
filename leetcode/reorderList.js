37170882a383246c4259120a1627b41792ce9393
Martin Kim <martinjwkim@gmail.com>
2021-02-24 15:11:09 -0800

    reorder list slow

--- leetcode/reorderList.js
+++ leetcode/reorderList.js
@@ -1,36 +1,25 @@
 const reorderList = (head) => {
-  let start = head;
-  let reorderedList = reverseList(head);
-  let count = 0;
-  let node = start;
+  let left = true;
 
-  while(node){
-    count++;
-    node = node.next;
+  let arr = [];
+  let curr = head;
+
+  while (curr){
+    arr.push(curr);
+    curr = curr.next;
   }
 
-  let res = new ListNode(0);
+  let start = new ListNode(0);
+  let node;
+  curr = start;
 
-  for (let i=0; i<count; i++){
-    if (i%2 === 1) {
-      res.next = reorderedList;
-      reorderedList = reorderedList.next;
-    } else {
-      res.next = start;
-      start = start.next;
-    }
-
-    res = res.next
+  while (arr.length){
+    curr.next = left ? arr.shift() : arr.pop()
+    left = !left;
+    curr = curr.next;
   }
 
-  return res;
+  curr.next = null;
+
+  return start.next;
 };
-
-const reverseList = (head, prev=null) => {
-  if (!head) return prev;
-
- let start = reverseList(head.next, head);
- head.next = prev;
-
-  return start;
-}
