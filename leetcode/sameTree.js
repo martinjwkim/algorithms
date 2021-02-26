f08fee0139cf4bee49e75b9771493f0378bcc59d
Martin Kim <martinjwkim@gmail.com>
2021-02-25 22:24:02 -0800

    same tree

---
+++ leetcode/sameTree.js
@@ -0,0 +1,7 @@
+const isSameTree = (p, q) => {
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
