d146c8378fd500e6d3566f2d33693822aa5896dc
Martin Kim <martinjwkim@gmail.com>
2021-02-26 22:01:02 -0800

    level order

--- leetcode/binaryTreeLevelOrderTraversal.js
+++ leetcode/binaryTreeLevelOrderTraversal.js
@@ -1,15 +1,15 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[][]}
- */
 var levelOrder = function(root) {
-    
+  if (!root) return []
+  const arr = [root], result = []
+  while (arr.length) {
+      const size = arr.length, values = []
+      for (let i = 0; i < size; i++) {
+          const node = arr.shift()
+          values.push(node.val)
+          if (node.left) arr.push(node.left) 
+          if (node.right) arr.push(node.right) 
+      }
+      result.push(values)
+  }
+  return result
 };
