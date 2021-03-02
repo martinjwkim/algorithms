21e9d3f3babbd18082f5bb7fd3d8c97150f3fc80
Martin Kim <martinjwkim@gmail.com>
2021-03-01 18:47:07 -0800

    kth smallest element in a bst

--- leetcode/kthSmallestElementInABST.js
+++ leetcode/kthSmallestElementInABST.js
@@ -1,15 +1,10 @@
 const kthSmallest = (root, k) => {
   const traverse = (node, travelled) => {
-    if (node.left){
-      traverse(node.left, travelled)
-    }
+    if (node.left) traverse(node.left, travelled);
     travelled.push(node);
-    if (node.right){
-      traverse(node.right, travelled)
-    }
-
+    if (node.right) traverse(node.right, travelled);
     return travelled;
   }
-
+  
   return traverse(root, [])[k-1].val;
 }
