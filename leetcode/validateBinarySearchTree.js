16a523da269ffb308500bfff8882e0711d6f19cf
Martin Kim <martinjwkim@gmail.com>
2021-03-01 16:09:06 -0800

    validate binary search tree

--- leetcode/validateBinarySearchTree.js
+++ leetcode/validateBinarySearchTree.js
@@ -1,3 +1,7 @@
-const isValidBST = (root) => {
-  
+const isValidBST = (node, low = -Infinity, high = Infinity) => {
+  if (!node) return true;
+  if (node.val <= low) return false;
+  if (node.val >= high) return false;
+
+  return isValidBST(node.left, low, node.val) && isValidBST(node.right, node.val, high)
 }
