bd78492b64870af9ae8015dcf11619869bf2c9b0
Martin Kim <martinjwkim@gmail.com>
2021-03-03 14:31:25 -0800

    design add and search words ds time limit exceeded

--- leetcode/designAddAndSearchWordsDS.js
+++ leetcode/designAddAndSearchWordsDS.js
@@ -8,36 +8,65 @@ class Node {
 
 class WordDictionary {
   constructor() {
+    this.list = [];
+  }
+
+  addWord(word) {
+    if (!this.list[word.length]) {
+      this.list[word.length] = new Trie();
+    }
+
+    this.list[word.length].insert(word);
+  }
+
+  search(word) {
+    if (!this.list[word.length]) return false;
+
+    return this.list[word.length].search(word);
+  }
+}
+
+class Trie {
+  constructor() {
     this.root = null;
   }
 
-  addWord(value, curr = this.root) {
+  insert(value, curr = this.root) {
+
     if (!this.root) {
       this.root = new Node(value);
-      return;
-    }
-
-    if (value < curr.val) {
-      if (curr.left) {
-        this.insert(value, curr.left)
-      } else {
-        curr.left = new Node(value);
-        return;
-      }
+      return this;
     } else {
-      if (curr.right) {
-        this.insert(value, curr.right)
+      if (value < curr.val) {
+        if (curr.left) {
+          this.insert(value, curr.left)
+        } else {
+          curr.left = new Node(value);
+          return this;
+        }
       } else {
-        curr.right = new Node(value);
-        return;
+        if (curr.right) {
+          this.insert(value, curr.right)
+        } else {
+          curr.right = new Node(value);
+          return this;
+        }
       }
+      return this;
     }
   }
 
-  search(value, curr=this.root) {
+  search(value, curr = this.root) {
     if (!curr) return false;
-    if (curr.val === value) return true;
-  
-    return value < curr.val ? this.search(value, curr.left) : this.search(value, curr.right)
+
+    let dot = false;
+
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] === '.') dot = true;
+      else if (dot && value[i] !== curr.val[i]) return this.search(value, curr.left) || this.search(value, curr.right)
+      else if (!dot && value[i] !== curr.val[i]) return value[i] < curr.val[i] ? this.search(value, curr.left) : this.search(value, curr.right)
+    }
+
+    return true;
   }
 }
