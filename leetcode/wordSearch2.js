160b4c5faf149c01a254c12e01f2d22608f3663c
Martin Kim <martinjwkim@gmail.com>
2021-03-05 20:02:56 -0800

    word search 2 slower solution

--- leetcode/wordSearch2.js
+++ leetcode/wordSearch2.js
@@ -2,36 +2,24 @@ const findWords = (board, words) => {
 
   const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
 
-  const buildTree = (words) => {
-    let tree = {};
+  const findWord = (x, y, curr) => {
 
-    for (let word of words) {
-      let node = tree;
-      for (let char of word) {
-        if (!node[char]) node[char] = {};
-        node = node[char];
-      }
-      node.word = word;
-    }
-
-    return tree;
-  }
-
-  const search = (x, y, node) => {
-
-    if (node.word) resSet.add(node.word);
+    if (!wordSet.size) return;
     if (x < 0 || y < 0 || x >= board[0].length || y >= board.length) return;
-    if (!node) return;
     if (board[y][x] === '#') return;
 
     let cell = board[y][x];
+    curr += cell;
 
-    if (!node[cell]) return;
+    if (wordSet.has(curr)) {
+      resSet.add(curr);
+      wordSet.delete(curr);
+    }
 
     board[y][x] = '#'
 
     for (let [dx, dy] of directions) {
-      search(x + dx, y + dy, node[cell]);
+      findWord(x + dx, y + dy, curr);
     }
 
     board[y][x] = cell;
@@ -39,19 +27,41 @@ const findWords = (board, words) => {
     return;
   }
 
-  let root = buildTree(words);
+  const filterImpossibleWords = (words) => {
+    let chars = new Set();
+    let possibleWords = new Set();
+    let valid;
+
+    for (let y = 0; y < board.length; y++) {
+      for (let x = 0; x < board[0].length; x++) {
+        chars.add(board[y][x]);
+      }
+    }
+
+    for (let word of words) {
+      valid = true;
+      for (let char of word) {
+        if (!chars.has(char)) valid = false;
+      }
+      if (valid) possibleWords.add(word);
+    }
+
+    return possibleWords;
+  }
+
+  let wordSet = filterImpossibleWords(words);
   let resSet = new Set();
   let res = [];
 
   for (let y = 0; y < board.length; y++) {
     for (let x = 0; x < board[0].length; x++) {
-      search(x, y, root)
+      findWord(x, y, '')
     }
   }
 
-  for(let word of resSet){
+  for (let word of resSet) {
     res.push(word)
   }
-  
+
   return res;
 }
