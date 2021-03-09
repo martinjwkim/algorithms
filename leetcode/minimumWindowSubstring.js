979bb38a6e93a78aa3bcf00e97e04f7ee1acafe1
Martin Kim <martinjwkim@gmail.com>
2021-03-09 04:03:36 -0800

    minimum window substring solution but needs cleaning up

--- leetcode/minimumWindowSubstring.js
+++ leetcode/minimumWindowSubstring.js
@@ -1,9 +1,9 @@
 const minWindow = (s,t) => {
   let start = 0;
   let end = 0;
-  let res = s;
+  let res = "";
 
-  let tCount = new Array(26).fill(0);
+  let tCount = new Array(100).fill(0);
   let tSet = new Set();
 
   for (let char of t) {
@@ -11,31 +11,25 @@ const minWindow = (s,t) => {
     tSet.add(char);
   }
 
-  while (start < s.length-1){
+  while (end <= s.length){
+    let max = Math.max(...tCount)
     if (!tSet.has(s[start])){
       start++;
-    } else if (!tSet.has(s[end])){
-      end++;
+      if (end < start) end = start;
+    } else if (max > 0) {
       if (tSet.has(s[end])){
         tCount[s[end].charCodeAt(0)-65]-=1
       }
-    } else if (Math.max(...tCount) === 0) {
-      let str = s.slice(start,end+1);
-      if (str.length < res.length) res = str;
+      end++;
+    } else if (max === 0){
+      let str = s.slice(start, end);
+      if (res === "" || str.length < res.length){
+        res = str;
+      }
       tCount[s[start].charCodeAt(0)-65]+=1;
       start++;
-    } else if (Math.max(...tCount)>0){
-      end++;
-      if (tSet.has(s[end])){
-        tCount[s[end].charCodeAt(0)-65]-=1
-      }
     }
   }
 
   return res;
-  
 }
-
-console.log(minWindow("ADOBECODEBANC",'ABC'))
-
-// s = "ADOBECODEBANC", t = "ABC"
