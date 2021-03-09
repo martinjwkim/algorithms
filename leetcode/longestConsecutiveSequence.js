2a4261db78d32814590405764cf4d70014d213d1
Martin Kim <martinjwkim@gmail.com>
2021-03-09 06:48:05 -0800

    longest consecutive sequence

--- leetcode/longestConsecutiveSequence.js
+++ leetcode/longestConsecutiveSequence.js
@@ -1,15 +1,27 @@
-const eraseOverlapIntervals = (nums) => {
-  let sortedNums = nums.sort((a, b) => a[1] - b[1]);
-  let count = 0;
-  let curr = sortedNums[0];
+const longestConsecutive = (nums) => {
+  let neg = Math.min(...nums) < 0 ? true : false;
+  let obj = {};
+  let res = nums.length ? 1 : 0;
+  let curr, prev;
 
-  for (let i = 1; i < sortedNums.length; i++) {
-    if (curr[1] > sortedNums[i][0]){
-      count++;
+  for (let num of nums) {
+    if (neg) {
+      obj[num-Math.min(...nums)] = true
     } else {
-      curr = sortedNums[i];
+      obj[num] = true;
     }
   }
 
-  return count;
+  for (let num in obj) {
+    if (+num === +prev+1){
+      curr++;
+      res = Math.max(res, curr)
+    } else {
+      curr = 1;
+    }
+
+    prev = num;
+  }
+
+  return res;
 }
