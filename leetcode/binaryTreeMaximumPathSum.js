dc87001cd5e6eb15c38688d9ce24b0a8cc2020fc
Martin Kim <martinjwkim@gmail.com>
2021-03-10 12:32:36 -0800

    binary tree max path sum

---
+++ leetcode/binaryTreeMaximumPathSum.js
@@ -0,0 +1,21 @@
+const maxPathSum = (root) => {
+
+  let max = root.val;
+
+  const search = (node) => {
+    if (!node) return -Infinity;
+    
+    let leftSum = search(node.left);
+    let rightSum = search(node.right);
+
+    let sum = Math.max(leftSum+node.val, rightSum+node.val, node.val)
+
+    max = Math.max(max, leftSum+rightSum+node.val, sum)
+
+    return sum;
+  }
+
+  max = Math.max(search(root), max);
+
+  return max;
+}
