cf75323fb04c445671d6aec85ac6a7eaa0e0475e
Martin Kim <martinjwkim@gmail.com>
2021-03-10 14:21:02 -0800

    serialize and deserialize binary tree time limit exceeded

--- leetcode/serializeAndDeserializeBinaryTree.js
+++ leetcode/serializeAndDeserializeBinaryTree.js
@@ -1,7 +1,21 @@
-const serialize = (root) => {
+const serialize = (root, n=0, data=[]) => {
+  if (!root) return JSON.stringify(data);
 
+  data[n] = root.val;
+
+  serialize(root.left, 2*n+1, data)
+  serialize(root.right, 2*n+2, data)
+
+  return JSON.stringify(data);
 }
 
-const deserialize = (data) => {
-  
+const deserialize = (data, n=0, vals=JSON.parse(data)) => {
+  if (n>vals.length-1) return null;
+  if (!vals[n] && vals[n]!==0) return null;
+
+  let node = new TreeNode(vals[n]);
+  node.left = deserialize(data, 2*n+1);
+  node.right = deserialize(data, 2*n+2);
+
+  return node;
 }
