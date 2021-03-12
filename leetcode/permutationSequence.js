dfd2aae8f7869a416b746bcffb050dcc1b1f689f
Martin Kim <martinjwkim@gmail.com>
2021-03-11 23:03:03 -0800

    permutation sequence brute force

--- leetcode/permutationSequence.js
+++ leetcode/permutationSequence.js
@@ -1,3 +1,34 @@
 const getPermutation = (n, k) => {
-  
+  const factorial = (num) => {
+    if (num === 1) return 1;
+    return num * factorial(num-1)
+  }
+
+  let numPermutations = factorial(n);
+  let permutations = [];
+
+  let nums = new Set();
+
+  for (let i=1; i<=n; i++){
+    nums.add(i);
+  }
+
+  const findPermutations = (numsSet, curr) => {
+    if (!numsSet.size){
+      permutations.push(curr);
+      return;
+    }
+
+    for (let i=1; i<=n; i++){
+      if (numsSet.has(i)){
+        numsSet.delete(i);
+        findPermutations(numsSet, curr+`${i}`)
+        numsSet.add(i);
+      }
+    }
+  }
+
+  findPermutations(nums, '')
+
+  return permutations[k-1];
 }
