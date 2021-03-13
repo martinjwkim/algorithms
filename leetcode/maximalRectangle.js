a918d9053ca3e877d50697525107d246412120e1
Martin Kim <martinjwkim@gmail.com>
2021-03-12 18:44:29 -0800

    max height and max width maximal rectangle

--- leetcode/maximalRectangle.js
+++ leetcode/maximalRectangle.js
@@ -2,15 +2,37 @@ const maximalRectangle = (matrix) => {
   
   
   const findLargestRectangle = (x, y) => {
+    let len = findLargestSquare(x, y, 1);
+
+    return Math.max(len*maxHeight(x, y, len, len), len*maxWidth(x, y, len, len))
+  }
+
+  const findLargestSquare = (x, y, len) => {
     
   }
 
-  const findLargestSquare = (x, y) => {
+  const maxHeight = (x, y, width, height) => {
+    if (y+height > matrix.length) return height;
 
+    for (let dx=0; dx<width; dx++){
+      if (matrix[y+height][x+dx] === 0){
+        return height;
+      }
+    }
+
+    return maxHeight(x, y, width, height+1);
   }
 
-  const extend = (x, y, len, direction) => {
+  const maxWidth = (x, y, width, height) => {
+    if (x+width > matrix[0].length) return width;
 
+    for (let dy=0; dy<height; dy++){
+      if (matrix[y+dy][x+width] === 0){
+        return width;
+      }
+    }
+
+    return maxHeight(x, y, width+1, height);
   }
 
   let maxArea = 0;
