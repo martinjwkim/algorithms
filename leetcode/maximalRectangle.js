755028385b6c6d3ecd20de1f0f902cc21628000b
Martin Kim <martinjwkim@gmail.com>
2021-03-12 18:13:47 -0800

    maximal rectangle base structure

--- leetcode/maximalRectangle.js
+++ leetcode/maximalRectangle.js
@@ -1,3 +1,26 @@
 const maximalRectangle = (matrix) => {
   
+  
+  const findLargestRectangle = (x, y) => {
+    
+  }
+
+  const findLargestSquare = (x, y) => {
+
+  }
+
+  const extend = (x, y, len, direction) => {
+
+  }
+
+  let maxArea = 0;
+
+  for (let j=0; j<matrix.length; j++){
+    for (let i=0; i<matrix[0].length; i++){
+      maxArea = Math.max(maxArea, findLargestRectangle(i, j));
+    }
+  }
+
+  return maxArea;
+
 }
