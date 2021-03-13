392899140ad4242c5ab128eff0d0522814fabc5e
Martin Kim <martinjwkim@gmail.com>
2021-03-12 17:49:56 -0800

    remove duplicates from sorted list 2

--- leetcode/removeDuplicatesFromSortedList2.js
+++ leetcode/removeDuplicatesFromSortedList2.js
@@ -1,3 +1,19 @@
 const deleteDuplicates = (head) => {
-  
+
+  let res = new ListNode('#');
+  res.next = head;
+
+  const skipDuplicates = (node, prev) => {
+    if (!node) return null;
+
+    if (prev === node.val || node.next?.val === node.val ) {
+      return skipDuplicates(node.next, node.val);
+    }
+
+    node.next = skipDuplicates(node.next, node.val)
+    
+    return node;
+  }
+
+  return skipDuplicates(res, '@').next;
 }
