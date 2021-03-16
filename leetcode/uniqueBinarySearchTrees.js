a88f3409a82bc74fbcb1f35003948a3317cc640e
Martin Kim <martinjwkim@gmail.com>
2021-03-16 13:12:35 -0700

    unique bst with recursion and memo

--- leetcode/uniqueBinarySearchTrees.js
+++ leetcode/uniqueBinarySearchTrees.js
@@ -1,36 +1,21 @@
-const numTrees = (n) => {
-  if (!n) return 0;
+const numTrees = (n, memo={}) => {
+  if (n < 2) return 1;
+  if (memo[n]) return memo[n];
+
   let count = 0;
-  let numSet = new Set();
 
-  for (let i = 1; i <= n; i++) {
-    numSet.add(i);
+  const findCombos = (curr, num) => {
+    let left = numTrees(curr-1, memo);
+    let right = numTrees(num-curr, memo);
+
+    return left*right
   }
 
-  const findCombos = (nums, left, right) => {
-    if (!nums.size){
-      count++;
-      return;
-    }
-
-    nums.forEach(num => {
-      nums.delete(num);
-
-      if (num > right && num > left){
-        findCombos(nums, left, num)
-      } else if (num < right && num > left) {
-        findCombos(nums, num, right)
-      }
-
-      nums.add(num)
-    })
+  for (let i=1; i<=n; i++) {
+    count+=findCombos(i, n);
   }
 
-  for (let i=1; i<=n; i++){
-    numSet.delete(i);
-    findCombos(numSet, i, i);
-    numSet.add(i);
-  }
+  memo[n] = count;
 
-  return count;
+  return memo[n];
 }
