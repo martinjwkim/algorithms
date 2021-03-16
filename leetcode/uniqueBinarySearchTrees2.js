3087bb19dccbfbb77ffc4a2ae6a35b4c4ac799a2
Martin Kim <martinjwkim@gmail.com>
2021-03-16 14:30:24 -0700

    unique binary search trees 2 with recursion and memoization

--- leetcode/uniqueBinarySearchTrees2.js
+++ leetcode/uniqueBinarySearchTrees2.js
@@ -1,18 +1,20 @@
 const generateTrees = (n, start=0, memo={}) => {
   if (n-start === 0) return [null];
-  if (n-start === 1) return [new ListNode(n)];
+  if (n-start === 1) return [new TreeNode(n)];
+  if (memo?.n?.start) return memo[n][start];
 
   let res = [];
 
   const buildTree = (num, high, low) => {
     let left = generateTrees(num-1, low, memo);
     let right = generateTrees(high, num, memo);
+
     let trees = [];
 
     for (let l of left){
-      let node = new ListNode(num);
-      node.left = l;
       for (let r of right) {
+        let node = new TreeNode(num);
+        node.left = l;
         node.right = r;
         trees.push(node)
       }
@@ -25,7 +27,8 @@ const generateTrees = (n, start=0, memo={}) => {
     res = res.concat(buildTree(i, n, start));
   }
 
-  memo[n] = res;
+  if (!memo[n]) memo[n]={};
+  memo[n][start] = res;
 
-  return memo[n];
+  return memo[n][start];
 }
