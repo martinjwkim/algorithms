4e0bb9c8d7b8b1c79448e88e89ce09a74ab0f263
Martin Kim <martinjwkim@gmail.com>
2021-03-16 13:14:42 -0700

    cleaned up

--- leetcode/uniqueBinarySearchTrees.js
+++ leetcode/uniqueBinarySearchTrees.js
@@ -1,18 +1,11 @@
-const numTrees = (n, memo={}) => {
+const numTrees = (n, memo = {}) => {
   if (n < 2) return 1;
   if (memo[n]) return memo[n];
 
   let count = 0;
 
-  const findCombos = (curr, num) => {
-    let left = numTrees(curr-1, memo);
-    let right = numTrees(num-curr, memo);
-
-    return left*right
-  }
-
-  for (let i=1; i<=n; i++) {
-    count+=findCombos(i, n);
+  for (let i = 1; i <= n; i++) {
+    count += numTrees(i - 1, memo) * numTrees(n - i, memo)
   }
 
   memo[n] = count;
