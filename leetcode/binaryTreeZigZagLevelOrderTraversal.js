77c4b0b85faf6dc4d93720a877f5b239d91c6cb1
Martin Kim <martinjwkim@gmail.com>
2021-03-19 23:40:55 -0700

    solution

--- leetcode/binaryTreeZigZagLevelOrderTraversal.js
+++ leetcode/binaryTreeZigZagLevelOrderTraversal.js
@@ -1,3 +1,27 @@
 const zigzagLevel = (root) => {
-  
+  if (!root) {
+    return [];
+} else {
+    const traversed = [[root.val]];
+    let parents = [root];
+    let children = [];
+    let zigZag = false;
+    while (parents.length > 0) {
+        for (let root of parents) {
+            if (root.left)
+                children.push(root.left);
+            if (root.right)
+                children.push(root.right);
+        }
+        if (children.length > 0) {
+            traversed.push(
+                zigZag ? children.map(child => child.val) : children.map(child => child.val).reverse()
+            );
+        }
+        zigZag = !zigZag;
+        parents = [ ...children ];
+        children = [];
+    }
+    return traversed;
+}
 }
